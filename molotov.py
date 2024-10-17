213ba45dfe62069f20ef178a50419460d2689740
M.-Leander Reimer <mario-leander.reimer@qaware.de>
2024-10-18 00:17:33 +0200

    Improved load tests.

--- molotov.py
+++ molotov.py
@@ -4,9 +4,21 @@ import molotov
 
 """
 Monolov load test for the QAware website.
-Weight is set to 100 for the scenario.
+Weight is set to 25 for the scenario.
 """
-@molotov.scenario(100)
+@molotov.scenario(25)
 async def scenario_qaware(session):
     async with session.get("https://www.qaware.de") as resp:
         assert resp.status == 200
+
+"""
+Monolov load test for the Microservice demo.
+Weight is set to 75 for the scenario.
+"""
+@molotov.scenario(75)
+async def scenario_demo(session):
+    async with session.get("https://learning-python-1091663571214.europe-north1.run.app") as resp:
+        assert resp.status == 200
+        
+        body = await resp.json()
+        assert body["message"] == "Hello World!"
